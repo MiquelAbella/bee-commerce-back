71a35df4f4f02f877616c691250bd289d0b61a34
Miquel <abellagarciamiquel@gmail.com>
2023-04-16 18:40:43 +0200

    changed cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const stripe = new Stripe(process.env.STRIPE_KEY);
 const cors = require("cors");
 app.use(
   cors({
-    origin: "https://bee-commerce-front.vercel.app",
+    origin: "https://main--illustrious-raindrop-0af8fd.netlify.app",
   })
 );
 app.use(bodyParse.urlencoded({ extended: true }));
